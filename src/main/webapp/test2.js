6c0541a4cebc456b8729ba6c42f0cb69417fae49
毛 <1296193245@qq.com>
2022-03-18 20:23:25 +0800

    JS_BOM

---
+++ src/main/webapp/test2.js
@@ -0,0 +1,12 @@
+/**
+ * Project name(项目名称)：JS_BOM
+ * File name(文件名): test2
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/3/18
+ * Time(创建时间)： 20:23
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+    
